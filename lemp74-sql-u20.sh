0e5c68abf82b9c7058d8fef36f9d8c6714cb3065
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-08-05 17:03:26 +0700

    Update lemp74-sql-u20.sh

--- lemp74-sql-u20.sh
+++ lemp74-sql-u20.sh
@@ -12,7 +12,7 @@ sudo ufw allow 'Nginx HTTP'
 sudo ufw allow 'Nginx HTTP'
 
 # Install PHP 7.4 Modules
-sudo apt install php7.4 php7.4-fpm php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl -y
+sudo apt install php7.4 php7.4-fpm php7.4-mysql php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl -y
 sudo systemctl restart nginx
 
 # Install MySQL Database Server
