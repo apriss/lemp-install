9ea48612850bbbfcdb1e0775ebbf14cd3fde5b2e
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-08-11 17:30:39 +0700

    Create lemp-rpm8.sh

---
+++ lemp-rpm8.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#INSTALL NGINX, MYSQL, PHP (LEMP-STACK) FOR RHEL 8 / ALAMA LINUX 8
+
+dnf update
+dnf install -y epel-release
+
+# Install Nginx Web Server
+dnf install -y nginx
+systemctl start nginx
+systemctl enable nginx
+
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
+
+# Install PHP 8
+dnf install -y dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
+dnf module reset php -y
+echo -n "Please select PHP version do want to install? (1. PHP 7.4, 2. PHP 8, 3. Skip) " 
+read ans
+case $ans in
+         1)
+	          dnf module enable php:remi-7.4 -y
+            apt install php7.4-{cgi,cli,curl,common,fpm,gd,json,ldap,mbstring,mysql,opcache,readline,redis,xml,zip}-y
+            systemctl enable php-fpm
+            systemctl start php-fpm
+            systemctl restart nginx
+	       ;;	
+              
+	       2)
+	          dnf module enable php:remi-8.0 -y
+            apt install php8.0-{cgi,cli,curl,common,fpm,gd,ldap,mbstring,mysql,opcache,readline,redis,xml,zip} -y
+            systemctl enable php-fpm
+            systemctl start php-fpm
+            systemctl restart nginx        
+	       ;;
+	      
+	       3)
+	          exit;;
+	      
+	       *)
+	          echo "Invalid Option"
+         ;;
+
+esac
+
+echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+
+# Install Database Server
+echo -n "Please select database server do want to install? (1. MariaDB, 2. Mysql, 3. Finish) " 
+read ans
+case $ans in
+        1)
+	         apt install mariadb-server -y
+           systemctl start mariadb
+           ssystemctl enable mariadb
+	         mysql_secure_installation
+	      ;;	
+        
+	      2)
+	         apt install mysql-server -y
+           systemctl start mysql
+           systemctl enable mysql
+	         mysql_secure_installation
+	      ;;
+	      
+	      3)
+	         exit;;
+	      
+	      *)
+	         echo "Invalid Option"
+        ;;
+
+esac
