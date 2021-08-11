3672ff9fc3022d094d4739b5b63951ac585baab1
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-08-11 16:52:03 +0700

    Update and rename lemp-deb.sh to lemp-focal.sh

--- lemp-focal.sh
+++ lemp-focal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#INSTALL NGINX, MYSQL/MARIADB, PHP74 (LEMP-STACK) FOR UBUNTU20/DEBIAN
+#INSTALL NGINX, MYSQL/MARIADB, PHP (LEMP-STACK) FOR UBUNTU20 20.04 
 
 sudo apt update
 
@@ -18,13 +18,13 @@ echo -n "Please select PHP version do want to install? (1. PHP 7.4, 2. PHP 8, 3.
 read ans
 case $ans in
          1)
-	    sudo apt install php7.4 php7.4-fpm php7.4-mysql php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-mbstring php7.4-xml php7.4-gd php7.4-curl -y
+	    sudo apt install php7.4-{cgi,cli,curl,common,fpm,gd,json,ldap,mbstring,mysql,opcache,readline,redis,xml,zip}-y
             sudo systemctl start php7.4-fpm
             sudo systemctl restart nginx
 	 ;;	
               
 	 2)
-	    sudo apt install php8.0 libapache2-mod-php8.0 php8.0-fpm libapache2-mod-fcgid php8.0-mysql php8.0-gd php8.0-common php8.0-cli php8.0-xml -y
+	    sudo apt install php8.0-{cgi,cli,curl,common,fpm,gd,ldap,mbstring,mysql,opcache,readline,redis,xml,zip} -y
             sudo systemctl start php8.0-fpm
             sudo systemctl restart nginx
 	 ;;
@@ -37,6 +37,8 @@ case $ans in
         ;;
 
 esac
+
+echo "<?php phpinfo(); ?>" > /var/www/html/info.php
 
 #STEP 3 INSTALL DATABASE SERVER
 echo -n "Please select database server do want to install? (1. MariaDB, 2. Mysql, 3 Finish) " 
