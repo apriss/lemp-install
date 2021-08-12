b064244e67392009074222129b5f27246b01769b
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-08-12 17:16:52 +0700

    Update lemp-rpm8.sh

--- lemp-rpm8.sh
+++ lemp-rpm8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#INSTALL NGINX, MYSQL, PHP (LEMP-STACK) FOR RHEL 8 / ALAMA LINUX 8
+#INSTALL NGINX, MYSQL/MARIADB, PHP (LEMP-STACK) FOR RHEL 8 / ALAMA LINUX 8
 
 dnf update
 dnf install -y epel-release
@@ -10,17 +10,18 @@ dnf install -y nginx
 systemctl start nginx
 systemctl enable nginx
 
+firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --reload
 
-# Install PHP 8
+# Install PHP Module
 dnf install -y dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
 dnf module reset php -y
 echo -n "Please select PHP version do want to install? (1. PHP 7.4, 2. PHP 8, 3. Skip) " 
 read ans
 case $ans in
          1)
-	    dnf module enable php:remi-7.4 -y
+	    dnf module enable php:remi-7.4 
             dnf install php-{cgi,cli,curl,common,fpm,gd,json,ldap,mbstring,mysql,opcache,readline,redis,xml,zip}-y
             systemctl enable php-fpm
             systemctl start php-fpm
