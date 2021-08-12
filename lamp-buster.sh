82b39c3ca30e51007ef2e3133e5a6bad3c6b403f
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-08-12 10:54:21 +0700

    Update lamp-buster.sh

--- lamp-buster.sh
+++ lamp-buster.sh
@@ -3,31 +3,31 @@
 #INSTALL APACHE, MYSQL / MARIADB, PHP FOR UBUNTU20 20.04
 
 #Step 1. INSTALL APACHE WEB SERVER
-sudo apt update
-sudo apt install apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
-sudo ufw allow 'Apache Secure'
+apt update
+apt install apache2 -y
+systemctl start apache2
+systemctl enable apache2
+ufw allow 'Apache Secure'
 
 #STEP 2 iNSTALL PHP MODULE
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php
-sudo apt update
+apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+apt update
 echo -n "Please select PHP version do want to install? (1. PHP 7.4, 2. PHP 8, 3. Skip) " 
 read ans
 case $ans in
          1)
-	    sudo apt install libapache2-mod-php7.4 php7.4-{cgi,cli,curl,common,fpm,gd,json,ldap,mbstring,mysql,opcache,readline,redis,xml,zip}-y
-            sudo systemctl restart apache2
-            sudo a2enmod proxy_fcgi setenvif
-            sudo a2enconf php7.4-fpm
+	    apt install libapache2-mod-php7.4 php7.4-{cgi,cli,curl,common,fpm,gd,json,ldap,mbstring,mysql,opcache,readline,redis,xml,zip}-y
+            systemctl restart apache2
+            a2enmod proxy_fcgi setenvif
+            a2enconf php7.4-fpm
 	 ;;	
               
 	 2)
-	    sudo apt install libapache2-mod-php8.0 php8.0-{cgi,cli,curl,common,fpm,gd,ldap,mbstring,mysql,opcache,readline,redis,xml,zip} -y
-            sudo systemctl restart apache2
-            sudo a2enmod proxy_fcgi setenvif
-            sudo a2enconf php8.0-fpm
+	    apt install libapache2-mod-php8.0 php8.0-{cgi,cli,curl,common,fpm,gd,ldap,mbstring,mysql,opcache,readline,redis,xml,zip} -y
+            systemctl restart apache2
+            a2enmod proxy_fcgi setenvif
+            a2enconf php8.0-fpm
 	 ;;
 	      
 	 3)
@@ -46,16 +46,16 @@ echo -n "Please select database server do want to install? (1. MariaDB, 2. Mysql
 read ans
 case $ans in
         1)
-	   sudo apt install mariadb-server -y
-           sudo systemctl start mariadb
-           sudo systemctl enable mariadb
+	   apt install mariadb-server -y
+           systemctl start mariadb
+           systemctl enable mariadb
 	   mysql_secure_installation
 	;;	
         
 	2)
-	   sudo apt install mysql-server -y
-           sudo systemctl start mysql
-           sudo systemctl enable mysql
+	   apt install mysql-server -y
+           systemctl start mysql
+           systemctl enable mysql
 	   mysql_secure_installation
 	;;
 	      
