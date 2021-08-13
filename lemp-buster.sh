4604ce02aa9e4b6a175be5ecf1a49fbec111eb19
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-08-13 16:39:53 +0700

    Update lemp-buster.sh

--- lemp-buster.sh
+++ lemp-buster.sh
@@ -7,11 +7,12 @@ apt update
 apt install nginx -y
 systemctl start nginx
 systemctl enable nginx
-ufw allow 'Apache Secure'
+sudo ufw allow 'Nginx HTTPS'
 
 #STEP 2 iNSTALL PHP MODULE
 apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 apt update
 echo -n "Please select PHP version do want to install? (1. PHP 7.4, 2. PHP 8, 3. Skip) " 
 read ans
@@ -19,12 +20,14 @@ case $ans in
          1)
 	    apt install php7.4-{cgi,cli,curl,common,fpm,gd,json,ldap,mbstring,mysql,opcache,readline,redis,xml,zip}-y
             systemctl start php7.4-fpm
+	    systemctl enable php-fpm
 	    systemctl restart nginx
 	 ;;	
               
 	 2)
 	    apt install php8.0-{cgi,cli,curl,common,fpm,gd,ldap,mbstring,mysql,opcache,readline,redis,xml,zip} -y
-            sudo systemctl start php8.0-fpm
+            systemctl start php8.0-fpm
+	    systemctl enable php8.0-fpm
 	    systemctl restart nginx
 	 ;;
 	      
