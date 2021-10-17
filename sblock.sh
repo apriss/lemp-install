65ab70c436da5213012e20fb29f6dfb59eebefe6
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-10-17 13:55:07 +0700

    Create sblock.sh
    Script for generate Nginx server block

---
+++ sblock.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Enter domain name : " domain
+
+cat > /etc/nginx/sites-available/$domain << EOF
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+}
+EOF
