aa9792e8c8ce43296b66f2c059e9d283a09c27a6
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-10-17 16:48:48 +0700

    Update sblock.sh

--- sblock.sh
+++ sblock.sh
@@ -10,12 +10,22 @@ server {
         root /var/www/html;
         index index.html index.htm index.nginx-debian.html;
 
-        server_name _;
+        server_name $domain;
 
         location / {
                 try_files $uri $uri/ =404;
         }
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/var/run/php/php8.0-fpm.sock;
+        }
+
+        location ~ /\.ht {
+                deny all;
+        }
+        
 }
 EOF
 
 ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
+unlink /etc/nginx/sites-enabled/default
