40374b2d6e34b6dcf5e0e866ec53224a7fae0da3
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-10-26 13:47:50 +0700

    Update sblock-deb.sh

--- nginx/sblock-deb.sh
+++ nginx/sblock-deb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+chown -R www-data:www-data /var/www
+
 read -p "Enter domain name : " domain
+
+doc="$document_root$fastcgi_script_name";
 
 cat > /etc/nginx/sites-available/$domain << EOF
 server {
@@ -16,7 +20,7 @@ server {
         
         location ~ \.php$ {
             fastcgi_pass unix:/run/php/php8.0-fpm.sock; 
-            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            fastcgi_param SCRIPT_FILENAME $doc;
             include fastcgi_params;
             include snippets/fastcgi-php.conf;
         }
@@ -31,4 +35,4 @@ EOF
 ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
 unlink /etc/nginx/sites-enabled/default
 
-chown -R www-data:www-data /var/www
+
