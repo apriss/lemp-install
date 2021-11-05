34a7af8ff79f2a0cb22b358c7f19ae8c7594edcb
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-11-05 14:04:34 +0700

    Create vhost-deb.sh

---
+++ apache/vhost-deb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Enter domain name : " domain
+
+cat > /etc/apache2/sites-available/$domain << EOF
+EOF<VirtualHost *:80>
+#  ServerAdmin webmaster@ostechnix1.lan
+   ServerName $domain
+   ServerAlias www.$domain
+   DocumentRoot /var/www/html/$domain
+ErrorLog ${APACHE_LOG_DIR}/error.log
+ CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
