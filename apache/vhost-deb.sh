2c4c76ee94e4fd213400df18fe733d2bc3a10d6a
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-11-10 14:57:55 +0700

    Update vhost-deb.sh

--- apache/vhost-deb.sh
+++ apache/vhost-deb.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 read -p "Enter domain name : " domain
+read -p "Enter server admin mail address : " sa
 
 cat > /etc/apache2/sites-available/$domain << EOF
-EOF<VirtualHost *:80>
-#  ServerAdmin webmaster@ostechnix1.lan
+<VirtualHost *:80>
+   ServerAdmin $sa
    ServerName $domain
    ServerAlias www.$domain
    DocumentRoot /var/www/$domain/public_html/
-ErrorLog ${APACHE_LOG_DIR}/error.log
- CustomLog ${APACHE_LOG_DIR}/access.log combined
+   DirectoryIndex index.php
+
+   <Directory /var/www/site1.your_domain>
+      Options Indexes FollowSymLinks MultiViews
+      AllowOverride All
+      Order allow,deny
+      allow from all
+   </Directory>
+
+   <FilesMatch \.php$>
+       SetHandler "proxy:unix:/run/php/php8.0-fpm.sock|fcgi://localhost"
+    </FilesMatch>
+    
+   ErrorLog ${APACHE_LOG_DIR}/error.log
+     CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 EOF
