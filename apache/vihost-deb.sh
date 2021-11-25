5dee4a4f7e3295705fa14b852fb05a398b3de8c6
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-11-25 14:11:09 +0700

    Update vihost-deb.sh

--- apache/vihost-deb.sh
+++ apache/vihost-deb.sh
@@ -32,31 +32,35 @@ case $ans in
         EOF
   ;;	
               
-	2)	      
-	      openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
-	      openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
-        cat > /etc/apache2/conf-available/ssl-params.conf << EOF
-          # from https://cipherli.st/
-          # and https://raymii.org/s/tutorials/Strong_SSL_Security_On_Apache2.html
+  2)	      
+	  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
+	  openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+          cat > /etc/apache2/conf-available/ssl-params.conf << EOF
+          	  # from https://cipherli.st/
+          	  # and https://raymii.org/s/tutorials/Strong_SSL_Security_On_Apache2.html
 
-          SSLCipherSuite EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH
-          SSLProtocol All -SSLv2 -SSLv3
-          SSLHonorCipherOrder On
-          # Disable preloading HSTS for now.  You can use the commented out header line that includes
-          # the "preload" directive if you understand the implications.
-          #Header always set Strict-Transport-Security "max-age=63072000; includeSubdomains; preload"
-          Header always set Strict-Transport-Security "max-age=63072000; includeSubdomains"
-          Header always set X-Frame-Options DENY
-          Header always set X-Content-Type-Options nosniff
-          # Requires Apache >= 2.4
-          SSLCompression off
-          SSLSessionTickets Off
-          SSLUseStapling on
-          SSLStaplingCache "shmcb:logs/stapling-cache(150000)"
+		  SSLCipherSuite EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH
+		  SSLProtocol All -SSLv2 -SSLv3
+		  SSLHonorCipherOrder On
+		  # Disable preloading HSTS for now.  You can use the commented out header line that includes
+		  # the "preload" directive if you understand the implications.
+		  #Header always set Strict-Transport-Security "max-age=63072000; includeSubdomains; preload"
+		  Header always set Strict-Transport-Security "max-age=63072000; includeSubdomains"
+		  Header always set X-Frame-Options DENY
+		  Header always set X-Content-Type-Options nosniff
+		  # Requires Apache >= 2.4
+		  SSLCompression off
+		  SSLSessionTickets Off
+		  SSLUseStapling on
+		  SSLStaplingCache "shmcb:logs/stapling-cache(150000)"
 
-          SSLOpenSSLConfCmd DHParameters "/etc/ssl/certs/dhparam.pem"
+		  SSLOpenSSLConfCmd DHParameters "/etc/ssl/certs/dhparam.pem"
         EOF
         
+	cat > /etc/apache2/sites-available/$domain.conf << EOF
+        	
+        EOF
+	
   ;;     
         
 	3)
